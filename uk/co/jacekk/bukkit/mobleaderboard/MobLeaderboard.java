2f514ae17f2e49554010f1ef0da0ccee65ab2c71
Jacek <jacek@kuzemczak.co.uk>
2012-05-23 22:40:18 +0100

    Added update time saving

--- uk/co/jacekk/bukkit/mobleaderboard/MobLeaderboard.java
+++ uk/co/jacekk/bukkit/mobleaderboard/MobLeaderboard.java
@@ -1,6 +1,12 @@
 package uk.co.jacekk.bukkit.mobleaderboard;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
 
 import uk.co.jacekk.bukkit.baseplugin.BasePlugin;
 import uk.co.jacekk.bukkit.baseplugin.config.PluginConfig;
@@ -18,7 +24,25 @@ public class MobLeaderboard extends BasePlugin {
 		this.config = new PluginConfig(new File(this.baseDirPath + File.separator + "config.yml"), ConfigKey.values(), this.log);
 		this.leaderboard = new Leaderboard(new File(this.baseDirPath + File.separator + "leaderboard.bin"));
 		
-		this.nextReset = System.currentTimeMillis();
+		File updateTimeFile = new File(this.baseDirPath + File.separator + "nextupdate.dat");
+		
+		if (!updateTimeFile.exists()){
+			this.nextReset = System.currentTimeMillis() + (this.config.getInt(ConfigKey.RESET_TIME) * 86400000);
+		}else{
+			try{
+				FileInputStream input = new FileInputStream(updateTimeFile);
+				DataInputStream stream = new DataInputStream(input);
+				BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+				
+				this.nextReset = Long.parseLong(reader.readLine().trim());
+				
+				reader.close();
+				stream.close();
+				input.close();
+			}catch (Exception e){
+				e.printStackTrace();
+			}
+		}
 		
 		try{
 			this.leaderboard.load();
@@ -47,6 +71,14 @@ public class MobLeaderboard extends BasePlugin {
 	public void onDisable(){
 		try{
 			this.leaderboard.save();
+			
+			FileWriter writer = new FileWriter(new File(this.baseDirPath + File.separator + "nextupdate.dat"));
+			BufferedWriter out = new BufferedWriter(writer);
+			
+			out.write(new StringBuilder().append(this.nextReset).toString());
+			
+			out.close();
+			writer.close();
 		}catch (Exception e){
 			e.printStackTrace();
 		}
